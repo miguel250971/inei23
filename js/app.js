895ea040e0d9bf9002225f9cc8c29a72628f600e
miguel250971 <i.mcabezas2019aip@gmail.com>
2026-07-05 19:53:36 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const hamburguesa = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+document.addEventListener('DOMContentLoaded',()=>{
+    mostrarMenu();
+    cerrarMenu();
+});
+
+function mostrarMenu(){
+    hamburguesa.addEventListener('click',()=>{
+        navegacion.classList.toggle('ocultar');
+    });
+}
+
+function cerrarMenu(){
+    enlaces.forEach(enlace =>{
+         enlace.addEventListener('click',(e)=>{
+        
+            if(e.target.tagName === 'A'){
+                navegacion.classList.add('ocultar');
+            }
+         }  
+    )})
+}
+
+
+
